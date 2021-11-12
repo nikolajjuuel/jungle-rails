a4cf70245f7cd2c5bcb7d93629d4d150b49a860b
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-11-12 00:03:17 +0000

    number of products and categories on dashboard with styling

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class Admin::DashboardController < ApplicationController
    http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
+
+
   def show
+    @all_products = Product.count()
+    @all_categories = Category.count()
   end
 
 end
